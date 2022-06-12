4301662c1877744b6300f2ec9dd95173f7d1a5d4
Luiz <eluiz8204@gmail.com>
2022-06-11 23:29:58 -0400

    novos estudos

---
+++ js-scripts/aula07/script07.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`o seu carro teve a vel de ${vel} km/h`)
+
+if(vel > 60){
+    console.log(`O seu carro ultrapassou a velocidade permitida`)
+}
+
+console.log('Digija sempre com cuidado!')
