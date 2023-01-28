da44bc4b224446dd85c48c43bce062fba29bb6e2
Luiz Eduardo <eluiz8204@gmail.com>
2023-01-28 00:57:25 -0300

    arrumando error js

--- section 2 - logica/004 - imc/assets/js/main.js
+++ section 2 - logica/004 - imc/assets/js/main.js
@@ -1,7 +1,29 @@
-formulario = document.querySelector('.form');
-resultado = document.querySelector('.resul');
+const formulario = document.querySelector('.form');
+formulario.addEventListener("submit", dadosIMC);
+
+const resultado = document.querySelector('.resul');
+
 function dadosIMC(evento) {
     evento.preventDefault();
-    const peso = formulario.querySelector('.peso');
-    console.log(peso);
+
+    let peso = (formulario.querySelector('.peso').value);
+    let altu = (formulario.querySelector('.altura').value);
+    
+    // verificando se o usuario digitou o seu peso em cm
+    if(Number.isInteger(altu)) {
+        altu = altu/100;
+        console.log(altu);
+    }
+
+    const formula = peso / altu ** 2
+
+    // testando os pesos e verificando aonde eles se encaixam
+
+
+    // verificando se há algum erro
+    if(!isNaN(formula) && isFinite(formula)) {
+        resultado.innerHTML = `<font style='background-color: blue; color: white;'>${formula}</font>`;
+    } else {
+        resultado.innerHTML = "<font style='background-color: red;'color='white'>Os dados são inválidos</font>";
+    }
 }
