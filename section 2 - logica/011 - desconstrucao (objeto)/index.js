f6a8648a3a9a50d51480a65a5a9e493ad2fd64b8
Luiz <eluiz8204@gmail.com>
2023-03-02 11:13:56 -0400

    Update index.js

--- section 2 - logica/011 - desconstrucao (objeto)/index.js
+++ section 2 - logica/011 - desconstrucao (objeto)/index.js
@@ -1,4 +1,5 @@
 // estrutura de um objeto
+
 const pessoa = {
     cpf: 1211231,
     nome: 'Luiz',
@@ -7,23 +8,22 @@ const pessoa = {
         cep: 78160805,
         rua: 5
     }
-}
+};
 
 // descontrução básica ( além de deixar um valor padrão para nome caso não exista essa var (se quisermos))
 // podemos mudar o nome também de tal var dentro do obj, como no caso em idade
 
 const { 
-    nome = 'Não existe', 
-    idade: anos, 
-    endereco, 
-    endereco: {
+    nome = 'Não existe', // aqui é o valor padrão dessa destruturaçãp
+    idade: anos, // aqui estamos passando outro nome como variavel 
+    endereco,  // essa var endereco serve para puxarmos o obj todo
+    endereco: { // desestruturação de um obj dentro de outro
         cep, 
         rua
-    } } = pessoa;
-
+    } } = pessoa; // aqui definimos da onde vamos puxar esses valores               
 
 // também podemos passar um valor que a partir dele, irá pegar todo o resto graças a '...'
-const {cpf, ... resto} = pessoa;
+const {cpf, ... resto} = pessoa;                                    
 
 // exibindo valores (exibindo o enderecço separado, já que podemos entender melhor)
 console.log(nome, anos, cep, rua);
